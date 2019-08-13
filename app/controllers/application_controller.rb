fca3078ef78ce67a28a12c01197878b5f66e80b0
Shanen Mays <co1627@yahoo.com>
2019-08-13 17:25:43 +0000

    made changes to application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,6 @@ class ApplicationController < ActionController::Base
    protect_from_forgery with: :exception
    
    def hello
-       render html: "Hello World!, seeing the changes"
+       render html: "Hello World!, making changes"
    end
 end
